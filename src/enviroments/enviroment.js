fa6c2521180cbc8673fdbe88e8b753f51b61ce4a
piotr dzienis <piotr.dzienis@inwedo.com>
2022-07-22 15:04:43 +0200

    fix: check env naming

--- src/enviroments/enviroment.js
+++ src/enviroments/enviroment.js
@@ -1,7 +1,7 @@
 export const enviroment = {
     apiUrl: 'https://api-football-v1.p.rapidapi.com/v3/',
     headers: {
-        'X-RapidAPI-Key': process.env.REACT_APP_VERSION_API_KEY,
-        'X-RapidAPI-Host': process.env.REACT_APP_VERSION_API_HOST
+        'X-RapidAPI-Key': process.env.REACT_APP_API_KEY,
+        'X-RapidAPI-Host': process.env.REACT_APP_API_HOST
     }
 }
